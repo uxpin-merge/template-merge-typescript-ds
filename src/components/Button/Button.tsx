e3e165301c13cea94a50651677f5276f459a9aae
Matthew Chigira <matthew@uxpin.com>
2021-03-15 15:31:26 +0900

    Change the Button.js component file to Button.tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+interface ButtonProps {
+  label: string;
+  disabled: boolean;
+  onClick(): void;
+}
+
+export default function Button(props: ButtonProps) {
+  return (
+    <button
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.label}
+    </button>
+  );
+}
