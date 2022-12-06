9ffc9f7d1f1180ac7238b95895f414f4008d8ff6
michaelrambeau <mikeair@gmail.com>
2022-12-06 11:50:44 +0900

    Add a type imported with `~`

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,20 @@
 import React from "react";
 import Button from "~/components/Button/Button";
+import { Size } from "~/types";
 
 type Props = {
   children: React.ReactNode;
+  size: Size;
 };
 export default function Card(props: Props) {
   return (
-    <div style={{ borderRadius: 8, border: "1px solid #bbb", padding: 10 }}>
+    <div
+      style={{
+        borderRadius: 8,
+        border: "1px solid #bbb",
+        padding: props.size === "large" ? 15 : 10,
+      }}
+    >
       <div>{props.children}</div>
       <Button label="Push me" />
     </div>
