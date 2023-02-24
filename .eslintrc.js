1b57ed3df2d902a7c20339f1016b8badb4fe31a5
Evan Trujillo <evantrujillo30@gmail.com>
2023-02-24 16:23:32 +0900

    Remove eslint disable comment and parse options for eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,10 +2,6 @@ module.exports = {
   root: true,
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint'],
-  parserOptions: {
-    sourceType: 'module',
-    ecmaVersion: 2020,
-  },
   extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended'],
   env: {
     browser: true,
--- src/components/UXPinWrapper/UXPinWrapper.tsx
+++ src/components/UXPinWrapper/UXPinWrapper.tsx
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-unused-vars
 import { ReactNode } from 'react';
 
 export default function UXPinWrapper({ children }: { children: ReactNode }) {
