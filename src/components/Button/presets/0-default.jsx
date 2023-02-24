545c369a36db5d070c73c954b45b18aa1f54ba43
Evan Trujillo <evantrujillo30@gmail.com>
2023-02-24 16:09:35 +0900

    Format files with prettier

--- src/components/Button/presets/0-default.jsx
+++ src/components/Button/presets/0-default.jsx
@@ -1,10 +1,4 @@
-import * as React from "react";
-import Button from "../Button";
+import * as React from 'react';
+import Button from '../Button';
 
-export default (
-  <Button
-    uxpId="button"
-    disabled={false}
-    label="Button">
-  </Button>
-);
+export default <Button uxpId="button" disabled={false} label="Button"></Button>;
