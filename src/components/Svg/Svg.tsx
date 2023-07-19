6608949a8726febf12c3520bb40e426d5928826a
Evan Trujillo <evantrujillo30@gmail.com>
2023-07-19 13:37:42 +0900

    Add SVG

---
+++ src/components/Svg/Svg.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Svg(props: any) {
+  return (
+    <div style={{ display: 'grid' }}>
+      <img src="/src/shared/assets/demo.svg" alt="A Rectangle Image with SVG" />
+    </div>
+  );
+}
